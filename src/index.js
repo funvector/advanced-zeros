daba2d39d82348ba6ebb575e19ee6004ecc1f649
Vlad K <huza007@mail.com>
2019-03-24 20:59:34 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let count, d, c, z = number;
+  for (let i = 2; i <= base; i++) {
+    if (base % i == 0) {
+      count = 0;
+      while (base % i == 0) {
+        count++;
+        base = Math.floor( base / i);
+      }
+      c = 0;
+      d = number;
+      while (d / i > 0) {
+        c += Math.floor(d / i);
+        d = Math.floor(d / i);
+      }
+      z = Math.min(z, Math.floor(c / count));
+    }
+  }
+  return z;
 }
